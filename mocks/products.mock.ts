906bb1505d4786a66a74dd3e866e6cfa3a2d95f7
Sándor Schwarz <sandor.schwarz89@gmail.com>
2023-10-09 16:45:25 +0200

    Add serverless

---
+++ mocks/products.mock.ts
@@ -0,0 +1,18 @@
+import { Product } from 'types/product.types';
+
+export const mockProducts: Product[] = [
+  {
+    id: '001',
+    title: 'Lotr',
+    description: 'string',
+    price: 10,
+    count: 0,
+  },
+  {
+    id: '002',
+    title: 'Hobbit',
+    description: 'string',
+    price: 10,
+    count: 0,
+  },
+];
